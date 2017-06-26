409c4abf7420c3369b93cc78eb18791996c8330f
KellyLaas <laas@iit.edu>
2017-06-26 16:20:30 -0500

    Wire up routes and actions and views for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  post 'auth/:provider/callback', to: 'sessions#create'
-
+  match 'auth/:provider/callback', to: 'sessions#create'
   resources :legs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'legs/index', to: 'legs#index', as: 'index'
   get 'legs/list', to: 'legs#list', as: 'list'
   post 'legs/index', to: 'legs#index'
+  get 'login', to: 'sessions#login', as: 'login'
+  get 'logout', to: 'sessions#logout', as: 'logout'
+  get 'auth/google_oauth2', as: 'google_login'
 
   root 'legs#index'
 
