fa1a6f750c9696d90f6222dda739f5dc748c2afb
KellyLaas <laas@iit.edu>
2017-06-29 17:43:38 -0500

    Add user.update = false to user model
    May want to have teammates check to see if this is correct

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :legs
   validates :name, :email, :uid, :community, :voucher, :vouched, presence: true
   validates :uid, uniqueness: true
+
+  before_update :set_user_not_updated 
+
+  def update
+    user.update = false
+  end
 end
